f16ebb7ea2d5dbe45a2542152d44df2401617eaf
nz663633 <hyunjisim3@gmail.com>
2026-01-10 19:26:46 +0900

    docs: study Node.js modules

---
+++ node_basic/module/func.js
@@ -0,0 +1,14 @@
+// 선언하지 않았지만 노드에서 require() 제공
+const { odd, even } = require('./var.js'); // 구조분해 할당
+
+function checkOddOrEven(number) {
+    if (number % 2) {
+        return odd;
+    } else {
+        return even;
+    }
+}
+
+// 이미 넘겨받은 변수를 또 넘겨줄 수 있다.
+// module.exports는 파일에서 한 번만 사용해야 한다. 
+module.exports = checkOddOrEven;
