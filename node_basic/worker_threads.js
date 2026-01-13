b92730a87844b72a6a173c8ca61594ffe38f60ae
nz663633 <hyunjisim3@gmail.com>
2026-01-13 22:04:52 +0900

    docs: study worker_threads with multiple workers

--- node_basic/worker_threads.js
+++ node_basic/worker_threads.js
@@ -1,17 +1,26 @@
 // worker_threads 멀티 스레드 사용
 
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+const { start } = require('repl');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
 if (isMainThread) { // 메인스레드
-    const worker = new Worker(__filename);
-    worker.on('message', (value) => console.log('워커로부터', value));
-    worker.on('exit', () => console.log('워커 끝~~'));
-
-    worker.postMessage('ping');
+    const threads = new Set();
+    threads.add(new Worker(__filename, { // 첫 번째 워커 생성
+        workerData: { start: 1 }, // 워커에게 초기 데이터 전달
+    }));
+    threads.add(new Worker(__filename, { // 두 번째 워커 생성
+        workerData: { start: 2 }, // 워커에게 초기 데이터 전달
+    }));
+    for (let worker of threads) {
+        worker.on('message', (value) => console.log('워커로부터', value));
+        worker.on('exit', () => {
+            threads.delete(worker);
+            if (threads.size === 0) {
+                console.log('워커 끝~~');
+            }
+        });
+    }
 } else { // 워커스레드
-    parentPort.on('message', (value) => {
-        console.log('부모로부터', value);
-        parentPort.postMessage('pong');
-        parentPort.close();
-    })
+    const data = workerData;
+    parentPort.postMessage(data.start + 100);
 }
