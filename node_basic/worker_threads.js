edfde68a9c35f5e9d67fc057f031a507e19f243c
nz663633 <hyunjisim3@gmail.com>
2026-01-13 21:44:55 +0900

    docs: study worker_threads basics

---
+++ node_basic/worker_threads.js
@@ -0,0 +1,17 @@
+// worker_threads 멀티 스레드 사용
+
+const { Worker, isMainThread, parentPort } = require('worker_threads');
+
+if (isMainThread) { // 메인스레드
+    const worker = new Worker(__filename);
+    worker.on('message', (value) => console.log('워커로부터', value));
+    worker.on('exit', () => console.log('워커 끝~~'));
+
+    worker.postMessage('ping');
+} else { // 워커스레드
+    parentPort.on('message', (value) => {
+        console.log('부모로부터', value);
+        parentPort.postMessage('pong');
+        parentPort.close();
+    })
+}
