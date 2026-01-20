ea3e08c4ddca96f77740b19a8e115e88a03aa66e
nz663633 <hyunjisim3@gmail.com>
2026-01-20 20:15:11 +0900

    docs: study creating an HTTP server with listen

---
+++ http_api/server1.js
@@ -0,0 +1,19 @@
+// 8080번 포트에서 서버를 실행하고
+// 클라이언트의 요청 받아 응답을 보냄
+// localhost:8080
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<p>Hello server</p>');
+    res.end('<p>Hello Hyeonji</p>');
+})
+    .listen(8080); // 서버를 8080번 포트에서 실행(요청 받을 준비완료)
+
+server.on('listening', () => {
+    console.log('8080번 포트에서 대기 중입니다.');
+});
+server.on('error', (error) => {
+    console.error(error);
+});
