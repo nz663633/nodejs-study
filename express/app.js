d4317a32de46192d0052efb1412df00e0ebdf323
nz663633 <hyunjisim3@gmail.com>
2026-01-25 18:55:24 +0900

    docs: study express middleware and routing

--- express/app.js
+++ express/app.js
@@ -1,4 +1,4 @@
-// 개발할 때 nodemon 서버를 더 자주 씀
+// 개발 환경에서는 주로 nodemon으로 서버 실행
 
 const express = require('express');
 const path = require('path');
@@ -9,12 +9,26 @@ app.set('port', process.env.PORT || 3000); // 전역변수처럼 접근 가능
 // process.env.PORT || 3000 : 값 역할
 // process.env.PORT : 서버 환경에서 미리 정해준 포트 번호
 
+app.use('/', (req, res, next) => { // 미들웨어(요청과 응답 사이에서 공통 로직 처리)
+    console.log('모든 요청에 실행하고 싶어요!');
+    next(); // 다음 미들웨어 또는 라우트 핸들러로 요청을 넘김, 호출하지 않으면 응답 안 넘어감
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/', (req, res) => { // if문을 쓰지 않아도 분기 처리됨
     res.send('hello express');
+});
+
+app.get('/category/Javascript', (req, res) => {
+    res.send(`hello Javascript`);
+});
+
+// 위에서 아래로 실행되므로 와일드 카드 매개변수는 마지막에 작성
+app.get('/category/:name', (req, res) => { // 라우트 매개변수 사용(URL 경로의 일부를 변수처럼 받아옴)
+    res.send(`hello ${req.params.name}`);
 });
 
 app.get('/about', (req, res) => {
