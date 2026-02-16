b283046b33fcb2a11d0e91a59f252228d7b91116
nz663633 <hyunjisim3@gmail.com>
2026-02-16 11:22:48 +0900

    docs: add comments

--- sns-service/controllers/page.js
+++ sns-service/controllers/page.js
@@ -13,9 +13,9 @@ exports.renderMain = async (req, res, next) => {
     const posts = await Post.findAll({
       include: {
         model: User,
-        attributes: ['id', 'nick'],
+        attributes: ['id', 'nick'], // 비밀번호는 프론트에 보내면 안되므로 적지 않음
       },
-      order: [['createdAt', 'DESC']],
+      order: [['createdAt', 'DESC']], // 가장 최신 게시물 먼저
     });
     res.render('main', {
       title: 'NodeBird',
@@ -35,11 +35,11 @@ exports.renderHashtag = async (req, res, next) => {
   try {
     const hashtag = await Hashtag.findOne({ where: { title: query } });
     let posts = [];
-    if (hashtag) {
+    if (hashtag) { // 해시태그로 게시물 찾기
       posts = await hashtag.getPosts({ include: [{ model: User }] });
     }
 
-    return res.render('main', {
+    return res.render('main', { // 찾은 게시물 렌더링
       title: `${query} | NodeBird`,
       twits: posts,
     });
@@ -48,3 +48,7 @@ exports.renderHashtag = async (req, res, next) => {
     return next(error);
   }
 };
+
+// 라우터 -> 컨트롤러 -> 서비스
+// 컨트롤러는 요청, 응답이 명확
+// 서비스는 요청, 응답이 불명확
